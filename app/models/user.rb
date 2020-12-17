b3fcbd5f7088d60c28acbf4f7cdb3492653a29f7
sayaka-ando <sayayann82@yahoo.co.jp>
2020-12-17 19:41:02 +0900

    itemsコントローラーの作成
    スタイルシートとビューファイルの整理も一緒に行った

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :email,              presence: true,   
-  validates :encrypted_password, presence: true,
+  validates :email,              presence: true
+  validates :encrypted_password, presence: true
   validates :nickname,           presence: true 
   validates :family_name,        presence: true
   validates :first_name,         presence: true
