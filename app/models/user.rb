4558e920ef5c56014cdcd2881d01b750217fb369
sayaka-ando <sayayann82@yahoo.co.jp>
2020-12-18 12:31:17 +0900

    ログインログアウト機能の実装
    新規登録情報のバリデーションも行った(漢字・かな・英数字)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :email,              presence: true
-  validates :encrypted_password, presence: true
-  validates :nickname,           presence: true 
+  validates :nickname,           presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do 
   validates :family_name,        presence: true
   validates :first_name,         presence: true
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/ } do
   validates :family_name_kana,   presence: true
   validates :first_name_kana,    presence: true
+  end
+  
   validates :birthday,           presence: true
+
+  has_many :items
+  has_many :buyers
 
 end
