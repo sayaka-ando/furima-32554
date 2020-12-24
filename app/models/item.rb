290bc5beb2be65ae6da82c0e95239daaa4a0f046
sayaka-ando <sayayann82@yahoo.co.jp>
2020-12-24 18:50:27 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :delivery_area_id
     validates :delivery_day_id
-    validates :price, format: { with: /d }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :user
   end
 
