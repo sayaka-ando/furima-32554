1ce04b4396f405de28987f97b00be2c427c2bb89
sayaka-ando <sayayann82@yahoo.co.jp>
2020-12-25 19:02:58 +0900

    activehashを使った項目の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,24 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
-  def item_params
-    params.require(:item).permit(:product, :image).marge(user_id: current_user.id)
-
   def index
     @items = Item.all
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product, :explanation, :category_id, :condition_id, :delivery_fee_id, :delivery_area_id, :delivery_day_id, :price, :image).marge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :product
     validates :explanation
-    validates :category_id
-    validates :condition_id
-    validates :delivery_fee_id
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :delivery_fee_id, numericality: { other_than: 1 }
     validates :delivery_area_id
-    validates :delivery_day_id
+    validates :delivery_day_id, numericality: { other_than: 1 }
     validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :user
   end
@@ -15,4 +15,12 @@ class Item < ApplicationRecord
   has_one :buyers
   belongs_to :users
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :delivery_area
+  belongs_to :delivery_day
+
 end
