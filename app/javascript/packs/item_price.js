d614ac9456253aa41733b42cb467d780248415ac
sayaka-ando <sayayann82@yahoo.co.jp>
2020-12-30 19:27:45 +0900

    consoleの削除

--- app/javascript/packs/item_price.js
+++ app/javascript/packs/item_price.js
@@ -2,17 +2,13 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    (inputValue);
 
     const addTaxDom = document.getElementById('add-tax-price')
     addTaxDom.innerHTML = (Math.floor(inputValue* 0.1));
-      console.log(addTaxDom)
 
       const profitNumber = document.getElementById('profit')
         const addTax = inputValue * 0.1
-        console.log(addTax)
         profitNumber.innerHTML = (Math.floor(inputValue - addTax));
-          console.log(profitNumber);
 
   })
 });
