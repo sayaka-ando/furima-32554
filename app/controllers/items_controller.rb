0c3af0a8a113eba5f10aabb38ab224fb9c47a5b9
sayaka-ando <sayayann82@yahoo.co.jp>
2021-01-05 14:56:54 +0900

    user_signed_in? && の削除
    ここはビフォーアクションとかぶっている内容だから

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless user_signed_in? && current_user.id == @item.user_id
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
@@ -45,5 +45,6 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+  
 
 end
