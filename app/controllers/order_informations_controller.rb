8fa635177ba82a905311ab25c0e8ef9763cfdd4b
sayaka-ando <sayayann82@yahoo.co.jp>
2021-01-07 00:02:26 +0900

    コントローラーの記述

--- app/controllers/order_informations_controller.rb
+++ app/controllers/order_informations_controller.rb
@@ -1,12 +1,25 @@
 class OrderInformationsController < ApplicationController
 
   def index
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
     @order_form = OrderForm.new
   end
 
   def create
-    binfing.pry
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(order_information_params)
+      if @order_form.valid?
+        @order_form.save
+        redirect_to root_path
+      else
+        render action: :index
+      end
+  end
+
+  private
+
+  def order_information_params
+    params.require(:order_form).permit(:user_id, :item_id, :postal_code, :delivery_area_id, :city, :address, :building, :phone_number, :order_information_id)
   end
   
 end
